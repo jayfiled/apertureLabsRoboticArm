9528341d6f7f153c607ea9c84b0f1a7ea907ba50
Joel James <jayfiled@gmail.com>
2019-06-26 22:33:58 +0900

    add router interactions, welcome, options and start to wire up to robot arm

---
+++ App/Controllers/RobotArm.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace apertureLabsRoboticArm
+{
+    class RobotArm
+    {
+    // Instance variables for current position [0,0]
+
+    // Each method will ask the view for input if necessary
+
+        // Method for place
+            // Creates the plate obj
+            // Checks the boundaries
+
+        // Method for Detect
+            // Checks for existence of plate obj
+            // Checks the boundaries
+
+        // Method for Drop
+            // Checks for existence of plate obj
+            // Checks the boundaries
+
+        // Method for Move n s e w
+            // Checks for existence of plate obj
+            // Checks the boundaries
+
+        // Method for report
+            // Checks for existence of plate obj
+            // Checks the boundaries
+
+        // Method to check the existence of plate Obj
+        
+        // Method to check the boundaries
+    }
+}
--- App/Router.cs
+++ App/Router.cs
@@ -4,17 +4,69 @@ namespace apertureLabsRoboticArm
 {
     class Router
     {
-        //Initialize an instance of the robot arm
+        // Set instance variables
+        public bool running = true;
+        RobotArm RoboArm = new RobotArm();
         public void Run()
         {
-            PrintWelcomeMessage();
+            while (running == true);
+            {
             Console.Write(PrintWelcomeMessage());
-            // Create a run function that:
-                // prints a welcome message
-                // shows the operator a list of commands they can do
-                // and loops while the user wants to keep using the arm
-            
-            // Function that executes the operators commands on the robot arm
+                // Create a run function that:
+                    // shows the operator a list of commands they can do
+                    // Save the chosen number to a variable
+                    // Run the chosen number over a switch case
+                    // That runs a method on the Robot arm instance
+            }
+            PrintFarewellMessage();
+        }
+
+        String[] actions = {
+            "PLACE the robot arm to get started",
+            "DETECT if the test tube is full or empty",
+            "DROP some solution in the test tube",
+            "MOVE the one position N, S, E or W",
+            "REPORT the current position and status of test tube below",
+            "EXIT operation of Robotic Arm"
+        };
+
+          private void RouteAction(action)
+          {
+            switch (action)
+            {
+                case 1:
+                    RoboArm.place;
+                    break;
+                case 2:
+                    RoboArm.detect;
+                    break;
+                case 3:
+                    RoboArm.drop;
+                    break;
+                case 4:
+                    RoboArm.move;
+                    break;
+                case 5:
+                    RoboArm.report;
+                    break;
+                case 6:
+                    stop();
+                    break;
+                default:
+                    Console.WriteLine("Please choose 1, 2, 3, 4, 5 or 6");
+                    break;
+            }
+          }
+        
+        private void DisplayMenu() 
+        {
+            // Loop over and print out all the actions, with 
+            // the operator presses 1, 2 or 3 etc to choose.
+        }
+
+        private void stop() 
+        {
+            running = false;
         }
 
         private String PrintWelcomeMessage()
@@ -23,7 +75,17 @@ namespace apertureLabsRoboticArm
                     @"
                     ------------
                     Welcome to Aperture labs
-                    Please select one of the following commands to get started.
+                    Please select one of the following commands:
+                    ------------"
+            );
+        }
+
+        private String PrintFarewellMessage()
+        {
+            return( 
+                    @"
+                    ------------
+                    Goodbye
                     ------------"
             );
         }
